be666393b0f983ab74caff49d72bd85258e165a7
Viktor Svyatokha <ahydrax@gmail.com>
2021-11-13 22:17:31 +1000

    net6 for test app

--- tests/Hangfire.MissionControl.Tests.Web/Program.cs
+++ tests/Hangfire.MissionControl.Tests.Web/Program.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Hangfire;
+using Hangfire.MemoryStorage;
+using Hangfire.MissionControl;
+using Hangfire.MissionControl.Tests.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Hangfire.MissionControl.Tests.Web
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<ITestSuite4, TestSuite4>();
+builder.Services.AddHangfire(configuration =>
 {
-    public static class Program
+    configuration.UseMemoryStorage();
+    configuration.UseMissionControl(
+        new MissionControlOptions
+        {
+            RequireConfirmation = false
+        },
+        typeof(TestSuite).Assembly);
+});
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+app.UseDeveloperExceptionPage();
+app.UseHangfireDashboard("/readonly",
+    new DashboardOptions
     {
-        public static void Main(string[] args) => 
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build()
-                .Run();
-    }
-}
+        IsReadOnlyFunc = ctx => true
+    });
+app.UseHangfireDashboard("",
+    new DashboardOptions
+    {
+        AppPath = "/readonly"
+    });
+
+app.Run();
